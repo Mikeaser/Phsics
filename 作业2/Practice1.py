18182d45ec6db287e55a1edc89593b52fc7a9d6c
SleepPenguin <1511467743@qq.com>
2023-03-02 10:04:54 +0800

    作业2

---
+++ 作业2/Practice1.py
@@ -0,0 +1,126 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+def j_0(x):
+    '''
+    0th order Bessel function
+    '''
+    y = math.sin(x)/x
+    return y
+
+
+def j_1(x):
+    '''
+    1st order Bessel function
+    '''
+    y1 = math.sin(x)/x**2
+    y2 = math.cos(x)/x
+    return y1-y2
+
+def j_0_star(x):
+    '''
+    Reciprocal 0th order Bessel function for backpropagation
+    '''
+    y = 0
+    return y
+
+def j_1_star(x):
+    '''
+    Reciprocal first order Bessel function
+    '''
+    y = x*1e-5   # here is converted according to the result of upward recursion
+    return y
+
+def BesselUpward(x_list, l_max):
+    '''
+    The specific calculation of the Bessel function using upward method
+    Args:
+        x_list (list[float]): The x array to be calculated
+        l_max (int): The highest order of the Bessel function to be computed
+    Returns:
+        DataSet: A two-dimensional array, each outer element represents the corresponding point value of the l-th order Bessel function
+    '''
+    DataSet = []  # A two-dimensional data set for storing curves
+    for l in range(l_max+1):
+        y_l = []
+        n = 0
+        for x in x_list:
+            if l == 0:
+                y = j_0(x)
+            if l == 1:
+                y = j_1(x)
+            if l >= 2:
+                # Solve using recurrence relation
+                y = (2*l-1)*DataSet[l-1][n]/x - DataSet[l-2][n]
+                n += 1
+            y_l.append(y)
+        DataSet.append(y_l)
+    return DataSet
+
+def BesselDownward(x_list, l_max):
+    '''
+    The specific calculation of the Bessel function using downward method
+    Args:
+        x_list (list[float]): The x array to be calculated
+        l_max (int): The highest order of the Bessel function to be computed
+    Returns:
+        DataSet: A two-dimensional array, each outer element represents the corresponding point value of the l-th order Bessel function
+    '''
+    DataSet = []  # A two-dimensional data set for storing curves
+    for k in range(l_max+2):
+        y_l = []
+        n = 0
+        for x in x_list:
+            if k == 0:
+                y = j_0_star(x)
+            if k == 1:
+                y = j_1_star(x)
+            if k >= 2:
+                # Solve using recurrence relation
+                y = (2*(l_max-k)+3)*DataSet[k-1][n]/x - DataSet[k-2][n]
+                n += 1
+            y_l.append(y)
+        DataSet.append(y_l)
+    return DataSet
+
+
+if __name__ == "__main__":
+    modes = ['upward', 'downward']
+    for mode in modes:
+        if mode == 'upward':
+            x_min = 1.0
+            x_max = 20.0
+            l_max = 30
+            step = 0.001
+
+            x_list = np.arange(x_min, x_max, step).tolist()
+            BesselDataset = BesselUpward(x_list, l_max)
+
+            # Use matplotlib to draw pictures
+            DrawNum = [2, 5, 10]
+            for l_draw in DrawNum:
+                plt.plot(x_list, BesselDataset[l_draw], label = "l = "+ str(l_draw))
+            plt.title('Bessel function -- upward iteration method')
+            plt.legend() 
+            plt.savefig('./作业2/ResultUpward.jpg') 
+            plt.close() 
+            # plt.show()
+        if mode == 'downward':
+            x_min = 0.001
+            x_max = 20.0
+            l_max = 30
+            step = 0.001
+
+            x_list = np.arange(x_min, x_max, step).tolist()
+            BesselDataset = BesselDownward(x_list, l_max)
+            # Use matplotlib to draw pictures
+            DrawNum = [2, 5, 10]
+            for l_draw in DrawNum:
+                k_draw = l_max + 1 - l_draw 
+                plt.plot(x_list, BesselDataset[k_draw], label = "l = "+ str(l_draw))
+            plt.title('Bessel function -- downward iteration method')
+            plt.legend() 
+            plt.savefig('./作业2/ResultDownward.jpg') 
+            plt.close()
